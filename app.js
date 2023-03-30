780795fdf6ea4524f653bb44e11843319001b37d
Seb887 <seb.dev@posteo.de>
2023-03-30 23:45:27 +0200

    update UI

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('TEST');
+'use strict';
+
+const newEntrySection = document.querySelector('.newEntryModalSection');
+const newEntryButton = document.querySelector('#newEntryButton');
+
+console.log(newEntryButton);
+
+newEntryButton.onclick = () => openModal();
+
+const openModal = () => {
+  newEntrySection.classList.remove('hidden');
+};
+const closeModal = () => {
+  newEntrySection.classList.add('hidden');
+};
