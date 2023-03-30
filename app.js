abbef6d63044d0be4165bfc3f0e33b6acac5079c
Seb887 <seb.dev@posteo.de>
2023-03-30 23:51:27 +0200

    first functions

--- app.js
+++ app.js
@@ -2,14 +2,25 @@
 
 const newEntrySection = document.querySelector('.newEntryModalSection');
 const newEntryButton = document.querySelector('#newEntryButton');
+const closeModalButton = document.querySelector('.closeModalButton');
+const overlay = document.querySelector('.overlay');
 
 console.log(newEntryButton);
 
 newEntryButton.onclick = () => openModal();
+closeModalButton.onclick = () => cancelInput();
+overlay.onclick = () => cancelInput();
 
 const openModal = () => {
   newEntrySection.classList.remove('hidden');
 };
 const closeModal = () => {
   newEntrySection.classList.add('hidden');
+};
+
+const clearInputs = () => {};
+
+const cancelInput = () => {
+  clearInputs();
+  closeModal();
 };
