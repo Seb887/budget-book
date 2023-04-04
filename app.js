9a583184f1ac452016491731263523e89d5cb988
Seb887 <seb.dev@posteo.de>
2023-04-05 00:30:04 +0200

    function createHTML

--- app.js
+++ app.js
@@ -4,12 +4,24 @@ const newEntrySection = document.querySelector('.newEntryModalSection');
 const newEntryButton = document.querySelector('#newEntryButton');
 const closeModalButton = document.querySelector('.closeModalButton');
 const overlay = document.querySelector('.overlay');
+const saveButton = document.querySelector('#saveButton');
 
-console.log(newEntryButton);
+const ulElement = document.querySelector('.liste');
 
+const inputTyp = document.querySelectorAll('.inputTyp');
+const inputDatum = document.querySelector('#inputDatum');
+const inputTitel = document.querySelector('#inputTitel');
+const inputBetrag = document.querySelector('#inputBetrag');
+
+// console.log(newEntryButton);
+
+let einraege = [];
+
+// Events
 newEntryButton.onclick = () => openModal();
 closeModalButton.onclick = () => cancelInput();
 overlay.onclick = () => cancelInput();
+saveButton.onclick = () => eintragHinzufuegen();
 
 const openModal = () => {
   newEntrySection.classList.remove('hidden');
@@ -18,96 +30,132 @@ const closeModal = () => {
   newEntrySection.classList.add('hidden');
 };
 
-const clearInputs = () => {};
+const clearInputs = () => {
+  inputDatum.value = '';
+  inputTitel.value = '';
+  inputBetrag.value = '';
+};
 
 const cancelInput = () => {
   clearInputs();
   closeModal();
 };
 
-//      <!-- List of Entries -->
-//       <div class="entriesContainer col-start-3 col-span-7 ml-10 mr-10">
-//         <!-- List of Month -->
-//         <div
-//           class="monthContainer space-y-4 mt-10 p-5 bg-slate-800 border border-slate-500 rounded-xl"
-//         >
-//           <header class="monthHeader flex justify-between">
-//             <h1 class="month font-extrabold text-xl text-slate-300">
-//               März 2023
-//             </h1>
-//             <span class="text-right font-medium text-xl text-green-500"
-//               >1781,55 €</span
-//             >
-//           </header>
+const createNewListItem = function (obj) {
+  const timestamp = Date.now();
 
-//           <!-- Entry -->
-//           <div
-//             class="newEntryContainer grid grid-cols-12 bg-slate-600 text-gray-300 rounded-lg"
-//           >
-//             <div
-//               class="inputDate p-2 col-span-2 text-center border-r border-slate-700"
-//             >
-//               07.03.2023
-//             </div>
-//             <div class="inputTitle p-2 pl-5 col-start-3 col-span-7 text-left">
-//               Einkauf
-//             </div>
-//             <div
-//               class="inputAmount p-2 pr-5 col-start-10 col-span-2 text-right text-red-500 font-medium border-r border-slate-700"
-//             >
-//               -143,44 €
-//             </div>
-//             <div
-//               class="removeEntry p-2 col-start-12 col-span-1 text-center text-gray-900"
-//             >
-//               <i class="fa-solid fa-trash"></i>
-//             </div>
-//           </div>
-//           <!-- Entry -->
-//           <div
-//             class="newEntryContainer grid grid-cols-12 bg-slate-600 text-gray-300 rounded-lg"
-//           >
-//             <div
-//               class="inputDate p-2 col-span-2 text-center border-r border-slate-700"
-//             >
-//               15.03.2023
-//             </div>
-//             <div class="inputTitle p-2 pl-5 col-start-3 col-span-7 text-left">
-//               Tanken
-//             </div>
-//             <div
-//               class="inputAmount p-2 pr-5 col-start-10 col-span-2 text-right text-red-500 font-medium border-r border-slate-700"
-//             >
-//               -75,01 €
-//             </div>
-//             <div
-//               class="removeEntry p-2 col-start-12 col-span-1 text-center text-gray-900"
-//             >
-//               <i class="fa-solid fa-trash"></i>
-//             </div>
-//           </div>
-//           <!-- Entry -->
-//           <div
-//             class="newEntryContainer grid grid-cols-12 bg-slate-600 text-gray-300 rounded-lg"
-//           >
-//             <div
-//               class="inputDate p-2 col-span-2 text-center border-r border-slate-700"
-//             >
-//               30.03.2023
-//             </div>
-//             <div class="inputTitle p-2 pl-5 col-start-3 col-span-7 text-left">
-//               Gehalt
-//             </div>
-//             <div
-//               class="inputAmount p-2 pr-5 col-start-10 col-span-2 text-right text-green-500 font-medium border-r border-slate-700"
-//             >
-//               2000 €
-//             </div>
-//             <div
-//               class="removeEntry p-2 col-start-12 col-span-1 text-center text-gray-900"
-//             >
-//               <i class="fa-solid fa-trash"></i>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
+  const listenelement = document.createElement('li');
+  obj.typ === 'ausgabe'
+    ? listenelement.classList.add('ausgabe')
+    : listenelement.classList.add('einnahme');
+  listenelement.classList.add(
+    'grid',
+    'grid-cols-12',
+    'bg-slate-600',
+    'text-gray-300',
+    'rounded-lg'
+  );
+  listenelement.setAttribute('data-timestamp', timestamp);
+
+  const datum = document.createElement('span');
+  datum.classList.add(
+    'eingabeDatum',
+    'p-2',
+    'col-span-2',
+    'text-center',
+    'border-r',
+    'border-slate-700'
+  );
+  console.log(obj.datum);
+  datum.textContent = obj.datum;
+
+  const titel = document.createElement('span');
+  titel.classList.add(
+    'eingabeTitel',
+    'p-2',
+    'pl-5',
+    'col-start-3',
+    'col-span-6',
+    'text-left'
+  );
+  titel.textContent = obj.titel;
+
+  const betrag = document.createElement('span');
+  betrag.classList.add(
+    'eingabeBetrag',
+    'p-2',
+    'pr-5',
+    'col-start-9',
+    'col-span-3',
+    'text-right',
+    // 'text-red-500',
+    'font-medium',
+    'border-r',
+    'border-slate-700'
+  );
+  if (obj.typ === 'ausgabe') {
+    betrag.classList.add('text-red-500');
+    betrag.textContent = `-${obj.betrag} €`;
+  } else {
+    betrag.classList.add('text-green-500');
+    betrag.textContent = `${obj.betrag} €`;
+  }
+
+  const eintragLöschen = document.createElement('button');
+  eintragLöschen.classList.add(
+    'eintragLöschen',
+    'p-2',
+    'col-start-12',
+    'col-span-1',
+    'text-center',
+    'text-gray-900'
+  );
+
+  const eintragLöschenIcon = document.createElement('i');
+  eintragLöschenIcon.classList.add('fa-solid', 'fa-trash');
+
+  eintragLöschen.appendChild(eintragLöschenIcon);
+  listenelement.appendChild(datum);
+  listenelement.appendChild(titel);
+  listenelement.appendChild(betrag);
+  listenelement.appendChild(eintragLöschen);
+  ulElement.appendChild(listenelement);
+
+  return listenelement;
+};
+
+const refreshHTML = function () {
+  clearList();
+  einraege.forEach((e) => createNewListItem(e));
+};
+
+const eintragHinzufuegen = function () {
+  let typ;
+
+  inputTyp.forEach((e) => {
+    if (e.checked === true && e.id === 'ausgabe') {
+      console.log(e.checked + e.id);
+      typ = 'ausgabe';
+    } else {
+      typ = 'einnahme';
+    }
+  });
+
+  const neuerEintrag = {
+    typ: typ,
+    datum: inputDatum.value,
+    titel: inputTitel.value,
+    betrag: inputBetrag.value,
+  };
+
+  einraege.push(neuerEintrag);
+
+  cancelInput();
+  refreshHTML();
+};
+
+const clearList = () => {
+  while (ulElement.firstChild) {
+    ulElement.removeChild(ulElement.firstChild);
+  }
+};
