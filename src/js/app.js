7d1a0e919adfa8b32359f15984bce8a6a08719f7
Seb887 <seb.dev@posteo.de>
2023-10-05 22:56:12 +0200

    remove item from list and local storage

--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,42 @@
 'use strict';
 
-// console.log('HELLO WORLD');
+///////////////
+// TEST AREA //
+///////////////
+
+// console.log('TEST: Hello World');
+
+// const newDate = new Date(Date.UTC(2012, 11, 20, 3, 0, 0)).toLocaleDateString(
+//   'de-DE',
+//   {
+//     year: 'numeric',
+//     month: '2-digit',
+//     day: '2-digit',
+//   }
+// );
+// console.log(newDate);
 
 // const modal = require('Modal');
 
 // -----------------------------------------------------------
 
-// VARIABLES
+// --- VARIABLES ---
 
-const monthList = document.querySelector('.monthList');
+const list = document.querySelector('.list');
 
 const inputDate = document.querySelector('#input-date');
 const inputTitle = document.querySelector('#input-title');
 const inputAmount = document.querySelector('#input-amount');
 const inputTyp = document.querySelectorAll('#input-typ');
 const submitBtn = document.querySelector('#submit-btn');
+const timestamp = Date.now();
 
 let entries = [];
 
 // --- FUNCTIONS ---
 
-// HTML für neuen Listeneintrag erstellen
+// Create HTML for list element
 const createNewListItem = (obj) => {
-  const timestamp = Date.now();
   const listItem = document.createElement('li');
   // obj.typ === 'ausgabe'
   //   ? listItem.classList.add('ausgabe')
@@ -40,21 +54,23 @@ const createNewListItem = (obj) => {
   const date = document.createElement('span');
   date.classList.add(
     'p-2',
-    'w-2/12',
+    'w-auto',
     'text-center',
     'border-r',
     'border-slate-700'
   );
   date.id = 'elementDate';
-  // date.textContent = obj.date.toLocaleDateString('de-DE', {
-  //   year: 'numeric',
-  //   month: '2-digit',
-  //   day: '2-digit',
-  // });
   date.textContent = obj.date;
 
   const title = document.createElement('span');
-  title.classList.add('elementTitle', 'p-2', 'w-7/12', 'pl-5', 'text-left');
+  title.classList.add(
+    'elementTitle',
+    'p-2',
+    'w-7/12',
+    'pl-2',
+    'text-left',
+    'font-bold'
+  );
   title.textContent = obj.title;
 
   const amount = document.createElement('span');
@@ -75,28 +91,119 @@ const createNewListItem = (obj) => {
   //   amount.classList.add('text-green-500');
   //   amount.textContent = `${obj.amount.toFixed(2).replace(/\./, ',')} €`;
   // }
-  amount.textContent = `-${obj.amount}`;
+  amount.textContent = amount.textContent = `-${obj.amount} €`;
 
-  const eintragLöschen = document.createElement('button');
-  eintragLöschen.classList.add(
+  // const removeElement = document.createElement('button');
+  // removeElement.classList.add(
+  // 'removeElement', 'p-2', 'text-center', 'text-gray-900';
+  // );
+
+  const removeIcon = document.createElement('i');
+  removeIcon.classList.add(
+    'fa-solid',
+    'fa-trash',
     'removeElement',
+    'flex',
+    'items-center',
     'p-2',
     'text-center',
     'text-gray-900'
   );
 
-  const eintragLöschenIcon = document.createElement('i');
-  eintragLöschenIcon.classList.add('fa-solid', 'fa-trash');
-
-  eintragLöschen.appendChild(eintragLöschenIcon);
   listItem.appendChild(date);
   listItem.appendChild(title);
   listItem.appendChild(amount);
-  listItem.appendChild(eintragLöschen);
-  monthList.appendChild(listItem);
+  listItem.appendChild(removeIcon);
+  list.appendChild(listItem);
 
   return listItem;
 };
+
+function addEntryToLocalStorage() {
+  const entriesFromStorage = getEntriesFromLocalStorage();
+
+  const newObj = {
+    title: inputTitle.value,
+    date: new Date(inputDate.value).toLocaleDateString('de-DE', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+    }),
+    amount: parseFloat(inputAmount.value).toFixed(2).replace(/\./, ','),
+    timestamp: timestamp,
+  };
+
+  entriesFromStorage.push(newObj);
+  localStorage.setItem('entries', JSON.stringify(entriesFromStorage));
+  console.log(entriesFromStorage);
+
+  refreshHTML();
+}
+
+function getEntriesFromLocalStorage() {
+  let entriesFromStorage;
+
+  if (localStorage.getItem('entries') === null) {
+    entriesFromStorage = [];
+  } else {
+    entriesFromStorage = JSON.parse(localStorage.getItem('entries'));
+  }
+
+  return entriesFromStorage;
+}
+
+function removeFromStorage(e) {
+  let entriesFromStorage = getEntriesFromLocalStorage();
+
+  if (e.target.classList.contains('removeElement')) {
+    let removeItemTitle = e.target.parentElement.children[1].textContent;
+    // let removeItemTimestamp = e.target.parentElement.timestamp;
+
+    entriesFromStorage = entriesFromStorage.filter(
+      (item) => removeItemTitle != item.title
+      // removeItemTimestamp != item.timestamp
+    );
+
+    localStorage.setItem('entries', JSON.stringify(entriesFromStorage));
+  }
+
+  refreshHTML();
+}
+
+function clearList() {
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+}
+function clearInputs() {
+  inputTitle.value = '';
+  // inputDate.value = '';
+  inputAmount.value = '';
+}
+
+// HTML aktualisieren
+const refreshHTML = () => {
+  clearList();
+  clearInputs();
+
+  // if (entries.length === 0) {
+  //   monthList.style.display = 'none';
+  // }
+
+  const entriesFromStorage = getEntriesFromLocalStorage();
+
+  entriesFromStorage.forEach((e) => createNewListItem(e));
+};
+
+// EVENTS
+submitBtn.addEventListener('click', addEntryToLocalStorage);
+list.addEventListener('click', removeFromStorage);
+
+window.onload = refreshHTML();
+
+/////////////////////////
+// FÜR SPÄTER AUFHEBEN //
+/////////////////////////
 
 // // Listenelemente sortieren
 // const eintraegeSort = () => {
@@ -115,36 +222,3 @@ const createNewListItem = (obj) => {
 //   });
 //   return eintraege;
 // };
-
-function addElement() {
-  const newObj = {
-    title: inputTitle.value,
-    date: inputDate.value,
-    amount: inputAmount.value,
-    // typ: inputTyp.value,
-  };
-  entries.push(newObj);
-  console.log(entries);
-
-  refreshHTML();
-}
-
-function clearList() {
-  monthList.innerHTML = '';
-}
-
-// HTML aktualisieren
-const refreshHTML = () => {
-  clearList();
-
-  if (entries.length === 0) {
-    monthList.style.display = 'none';
-  }
-
-  entries.forEach((e) => createNewListItem(e));
-};
-
-// EVENTS
-submitBtn.addEventListener('click', addElement);
-
-window.onload = refreshHTML();
